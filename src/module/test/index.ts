ddcfa06b9a0f4d318c2ac1902bddf8d93375d298
goupeng <2953854684@qq.com>
2023-10-10 23:06:20 +0800

    先写一个页面

--- src/module/test/index.ts
+++ src/module/test/index.ts
@@ -2,7 +2,7 @@
  * @Author: goupeng 2953854684@qq.com
  * @Date: 2023-10-10 23:02:31
  * @LastEditors: goupeng 2953854684@qq.com
- * @LastEditTime: 2023-10-10 23:03:30
+ * @LastEditTime: 2023-10-10 23:05:41
  * @FilePath: \project2c:\Users\15747\Desktop\nest-study\nest-study\src\module\test\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
